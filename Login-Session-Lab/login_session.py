0d9474b5ea2de1a9b8b703306a184c6e256dd545
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2022-07-28 15:44:12 +0300

    try

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -2,12 +2,24 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask import session as login_session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super-secret-key'
+app.config['SECRET_KEY'] = 'ufdsvzfghjk'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods = ['GET', 'POST']) # What methods are needed?
 def home():
-	
-	return render_template('home.html')
+	try:
+		if request.method == 'POST':
+			name = request.form['name']
+			login_session[name] = True
+			quote = request.form['quote']
+			login_session[quote] = True
+			age = request.form['age']
+			login_session[age] = True
+			return redirect (url_for('thanks'))
+
+		else:
+			return render_template('home.html')
+	except:
+		return redirect (url_for('error'))
 
 
 @app.route('/error')
@@ -19,7 +31,7 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', name = request.form[name]) # What variables are needed?
 
 
 @app.route('/thanks')
